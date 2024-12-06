54a3a9d96dbea2fd559aedd1688caf81f800a5f8
Kevin Reid <kpreid@switchb.org>
2024-12-05 21:59:26 -0800

    Add xtask and bacon config.
    This will (hopefully) speed up development with thorough testing
    (instead of repeatedly breaking docs or some feature combos).

---
+++ xtask/src/main.rs
@@ -0,0 +1,26 @@
+use xshell::{Cmd, Shell};
+
+// TODO: nice error formatting
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sh = &Shell::new()?;
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let args: Vec<&str> = args.iter().map(|s| &**s).collect();
+    match *args {
+        ["test"] => {
+            cargo(sh).args(["test", "--no-default-features"]).run()?;
+            cargo(sh).args(["test", "--features=std"]).run()?;
+            cargo(sh).args(["test", "--features=sync"]).run()?;
+            cargo(sh).args(["test", "--all-features"]).run()?;
+            cargo(sh).args(["doc"]).run()?;
+        }
+        _ => {
+            return Err(format!("invalid arguments: {args:?}").into());
+        }
+    }
+
+    Ok(())
+}
+
+fn cargo(sh: &Shell) -> Cmd {
+    sh.cmd(std::env::var("CARGO").expect("CARGO environment variable not set"))
+}
