0e745ee605e217fc493f2f8555fa0d556c1f4634
Kevin Reid <kpreid@switchb.org>
2025-08-21 12:30:46 -0700

    Add a test for `impl Stream for WakeFlag`.

--- tests/api/tools.rs
+++ tests/api/tools.rs
@@ -1,3 +1,9 @@
+use std::future::Future;
+
+use futures::task::LocalSpawnExt as _;
+
+// -------------------------------------------------------------------------------------------------
+
 /// Breaks the listener rules for testing by recording batch boundaries.
 #[derive(Debug)]
 pub(crate) struct CaptureBatch<L>(pub L);
@@ -9,4 +15,30 @@ where
     fn receive(&self, messages: &[M]) -> bool {
         self.0.receive(&[Vec::from(messages)])
     }
+}
+
+#[cfg_attr(not(feature = "async"), allow(dead_code))]
+pub(crate) async fn yield_now() {
+    let mut yielded = false;
+    core::future::poll_fn(move |ctx| {
+        if yielded {
+            core::task::Poll::Ready(())
+        } else {
+            yielded = true;
+            ctx.waker().wake_by_ref();
+            core::task::Poll::Pending
+        }
+    })
+    .await
+}
+
+/// Run a future to completion, but don't block if it doesn't wake, under the assumption that
+/// that indicates a lost-signal bug.
+///
+/// TODO: allow the future to borrow things and return a value
+#[cfg_attr(not(feature = "async"), allow(dead_code))]
+pub(crate) fn run_task_without_waiting(future: impl Future<Output = ()> + 'static) {
+    let mut executor = futures::executor::LocalPool::new();
+    executor.spawner().spawn_local(future).unwrap();
+    assert!(executor.try_run_one(), "Test stalled");
 }
